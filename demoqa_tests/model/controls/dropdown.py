05d843310680aad18428c1157d344bba6a6b697b
Fatalwgx <nothingfatalleo@gmail.com>
2022-09-21 17:57:31 +0300

    fixed dropdown compatabillity

--- demoqa_tests/model/controls/dropdown.py
+++ demoqa_tests/model/controls/dropdown.py
@@ -4,6 +4,6 @@ from selene import be, have
 
 def select(element, option):
     element.click()
-    browser.all('[id^=react-select][id*=-option-]').by(
+    browser.all('[id^=react-select][id*=-option-]').element_by(
         have.exact_text(f'{option}')
-    ).first.click()
+    ).click()
