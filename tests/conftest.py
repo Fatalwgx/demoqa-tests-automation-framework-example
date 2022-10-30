c8532f01df314a93d117b95b77764058b7413cf1
Fatalwgx <nothingfatalleo@gmail.com>
2022-10-30 17:24:12 +0300

    credentials setup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,4 @@
 import os
-
 import pytest
 from dotenv import load_dotenv
 from selene.support.shared import browser
@@ -42,7 +41,7 @@ def setup_browser(request):
     options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser.config.driver = driver
