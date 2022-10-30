1d76ec9d24b4ea625f15e3695218c19d703ba10d
Fatalwgx <nothingfatalleo@gmail.com>
2022-10-30 19:57:31 +0300

    steps for practice page added

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,5 +1,7 @@
 import datetime
 
+import allure
+
 from demoqa_tests.model import app
 from demoqa_tests.model.data import user
 from demoqa_tests.model.data.user import Gender
@@ -9,21 +11,26 @@ from demoqa_tests.model.pages import practice_page
 def test_submit_filled_form():
 
     # GIVEN
-    app.given_opened('/automation-practice-form')
+    with allure.step('Opening "Practice form" page '):
+        app.given_opened('/automation-practice-form')
 
     # WHEN
-    practice_page.fill_fullname('Lev', 'Zavodskov')
-    practice_page.fill_email('email@example.com')
-    practice_page.fill_gender(Gender.Male)
-    practice_page.fill_phone('70000000000')
-    practice_page.fill_birthday(datetime.date(1997, 7, 29))
-    practice_page.fill_subjects(user.Subjects.Math, user.Subjects.English, user.Subjects.Computer_science)
-    practice_page.fill_hobbies(user.Hobby.Music, user.Hobby.Reading)
-    practice_page.select_picture('picture.png')
-    practice_page.fill_address('Country/State/City/Street/ Street num')
-    practice_page.select_state('NCR')
-    practice_page.select_city('Gurgaon')
-    practice_page.submit_form()
+    with allure.step('Filling in the form with user Lev\'s data'):
+        practice_page.fill_fullname('Lev', 'Zavodskov')
+        practice_page.fill_email('email@example.com')
+        practice_page.fill_gender(Gender.Male)
+        practice_page.fill_phone('70000000000')
+        practice_page.fill_birthday(datetime.date(1997, 7, 29))
+        practice_page.fill_subjects(user.Subjects.Math, user.Subjects.English, user.Subjects.Computer_science)
+        practice_page.fill_hobbies(user.Hobby.Music, user.Hobby.Reading)
+        practice_page.select_picture('picture.png')
+        practice_page.fill_address('Country/State/City/Street/ Street num')
+        practice_page.select_state('NCR')
+        practice_page.select_city('Gurgaon')
+
+    with allure.step('Submitting the form'):
+        practice_page.submit_form()
 
     # THEN
-    practice_page.assert_form_sent()
+    with allure.step('Validating sent data'):
+        practice_page.assert_form_sent()
